c97210d742e755ad84641c13721a106b5a960803
Mstr-K <wambuakennedy467@gmail.com>
2025-05-26 20:30:24 +0300

    updated the code

--- script.js
+++ script.js
@@ -23,21 +23,27 @@ let index = 0;
 
 const activePortfolio = () => {
     const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
-    
-    imgSlide.style.transform = 'translateX(calc(${index * -100}% - ${index * 2}rem))';
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail');
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    portfolioDetails.forEach(detail => {
+        detail.classList.remove('active');
+    });
+    portfolioDetails[index].classList.add('active');
 }
 
 arrowRight.addEventListener('click', () => {
     if(index < 4){
         index++;
+        arrowLeft.classList.remove('disabled');
     }
     else{
         index = 5;
         arrowRight.classList.add('disabled');
     }
     activePortfolio();
-});
-
+    });
 arrowLeft.addEventListener('click',() => {
     if(index > 1){
         index--;
@@ -45,7 +51,7 @@ arrowLeft.addEventListener('click',() => {
     }
     else{
         index = 0;
-        arrowLeft.classList.remove('disabled');
+        arrowLeft.classList.add('disabled');
     }
     activePortfolio();
 });
